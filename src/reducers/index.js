f1ce5ffb76ed83ccce8843b3d1bf4da37d57f932
Ali Gasimzadeh <aligasimzadeh9@gmail.com>
2021-03-06 14:04:34 +0400

    redux todo-example

---
+++ src/reducers/index.js
@@ -0,0 +1,14 @@
+import { combineReducers } from 'redux';
+import todos from './todos';
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
+const persistConfig = {
+    key:'root',
+    storage
+}
+const rootReducer = combineReducers({
+	todos
+});
+
+export default persistReducer(persistConfig, rootReducer);
